a0600b7da116cbad863f39b110f85f0a063444ff
DevWedeloper <vicnathangabrielle@gmail.com>
2023-10-17 20:32:11 +0800

    Refactored typeEffectService code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,41 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { SectionService } from '../shared/data-access/section.service';
 import { TypeEffectService } from '../shared/data-access/type-effect.service';
 import { HighlightTextDirective } from '../shared/ui/directives/highlight-text.directive';
 
-interface TypingEffect {
-  phrases: string[];
-  typeSpeed: number;
-  reverseSpeed: number;
-  reverseDelay: number;
-  loop: boolean;
-}
-
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    imports: [HighlightTextDirective],
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-home',
+  standalone: true,
+  imports: [HighlightTextDirective],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
   ss = inject(SectionService);
   tes = inject(TypeEffectService);
   @ViewChild('section', { static: true }) section!: ElementRef<HTMLElement>;
-  @ViewChild('typeEffect', { static: true }) typeEffect!: ElementRef<HTMLElement>;
+  @ViewChild('typeEffect', { static: true })
+  typeEffect!: ElementRef<HTMLElement>;
 
   ngOnInit(): void {
     this.ss.registerSection(this.section);
-    const typingEffectOptions: TypingEffect = {
-      phrases: ['Software Engineer!', 'Full-stack Engineer!'],
-      typeSpeed: 55,
-      reverseSpeed: 40,
-      reverseDelay: 1000,
-      loop: true,
-    };
-    this.tes.addTypeEffect(typingEffectOptions, this.typeEffect.nativeElement);
+    this.tes.addTypeEffect(
+      {
+        phrases: ['Software Engineer!', 'Full-stack Engineer!'],
+        typeSpeed: 55,
+        reverseSpeed: 40,
+        reverseDelay: 1000,
+        loop: true,
+      },
+      this.typeEffect.nativeElement
+    );
   }
 }
--- src/app/shared/data-access/type-effect.service.ts
+++ src/app/shared/data-access/type-effect.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2, inject } from '@angular/core';
 
 interface TypingEffect {
   phrases: string[];
@@ -9,23 +9,28 @@ interface TypingEffect {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TypeEffectService {
   private renderer: Renderer2;
-  constructor(private rendererFactory: RendererFactory2) {
+  rendererFactory = inject(RendererFactory2);
+  constructor() {
     this.renderer = this.rendererFactory.createRenderer(null, null);
   }
 
-  addTypeEffect(typingEffectOptions: TypingEffect, element: HTMLElement): void {
+  addTypeEffect(
+    { phrases, typeSpeed, reverseSpeed, reverseDelay, loop }: TypingEffect,
+    element: HTMLElement
+  ): void {
     let letterIndex = 0;
     let phraseIndex = 0;
     let reverse = false;
 
-    const updateText = (text: string) => this.renderer.setProperty(element, 'textContent', text);
+    const updateText = (text: string) =>
+      this.renderer.setProperty(element, 'textContent', text);
 
     const typeWriter = () => {
-      const currentText = typingEffectOptions.phrases[phraseIndex];
+      const currentText = phrases[phraseIndex];
 
       if (reverse) {
         updateText(currentText.substring(0, letterIndex));
@@ -39,23 +44,23 @@ export class TypeEffectService {
         updateText(currentText.substring(0, letterIndex + 1));
         letterIndex++;
         if (letterIndex === currentText.length) {
-          if (phraseIndex === typingEffectOptions.phrases.length - 1 && !typingEffectOptions.loop) {
+          if (phraseIndex === phrases.length - 1 && !loop) {
             return;
           }
           setTimeout(() => {
             reverse = true;
             typeWriter();
-          }, typingEffectOptions.reverseDelay);
+          }, reverseDelay);
           return;
         }
       }
-      setTimeout(typeWriter, reverse ? typingEffectOptions.reverseSpeed : typingEffectOptions.typeSpeed);
+      setTimeout(typeWriter, reverse ? reverseSpeed : typeSpeed);
     };
 
     function moveToNextText(): void {
       phraseIndex++;
-      if (phraseIndex === typingEffectOptions.phrases.length) {
-        if (typingEffectOptions.loop) {
+      if (phraseIndex === phrases.length) {
+        if (loop) {
           phraseIndex = 0;
         } else {
           return;
