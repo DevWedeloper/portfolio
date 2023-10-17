1b83a59f49b301094ab4cac242413d62167ab9ef
DevWedeloper <vicnathangabrielle@gmail.com>
2023-10-17 20:21:13 +0800

    Refactored home component code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { SectionService } from '../shared/data-access/section.service';
 import { TypeEffectService } from '../shared/data-access/type-effect.service';
 import { HighlightTextDirective } from '../shared/ui/directives/highlight-text.directive';
@@ -20,16 +20,13 @@ interface TypingEffect {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
+  ss = inject(SectionService);
+  tes = inject(TypeEffectService);
   @ViewChild('section', { static: true }) section!: ElementRef<HTMLElement>;
   @ViewChild('typeEffect', { static: true }) typeEffect!: ElementRef<HTMLElement>;
 
-  constructor(
-    private sectionService: SectionService,
-    private typeEffectService: TypeEffectService
-  ){}
-
   ngOnInit(): void {
-    this.sectionService.registerSection(this.section);
+    this.ss.registerSection(this.section);
     const typingEffectOptions: TypingEffect = {
       phrases: ['Software Engineer!', 'Full-stack Engineer!'],
       typeSpeed: 55,
@@ -37,7 +34,6 @@ export class HomeComponent implements OnInit {
       reverseDelay: 1000,
       loop: true,
     };
-    this.typeEffectService.addTypeEffect(typingEffectOptions, this.typeEffect.nativeElement);
+    this.tes.addTypeEffect(typingEffectOptions, this.typeEffect.nativeElement);
   }
-
 }
