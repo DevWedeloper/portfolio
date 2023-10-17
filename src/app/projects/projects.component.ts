b51849ac6e7f38e6dd7d1e95f72d55accb26f86a
DevWedeloper <vicnathangabrielle@gmail.com>
2023-10-17 15:51:36 +0800

    Created card component, moved the necessary code, made the necessary input

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,19 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { SectionService } from '../shared/data-access/section.service';
-import { ThemeService } from '../shared/data-access/theme.service';
+import { CardComponent } from './ui/card/card.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectsComponent implements OnInit {
   ss = inject(SectionService);
-  ts = inject(ThemeService);
   @ViewChild('section', { static: true }) section!: ElementRef<HTMLElement>;
 
   ngOnInit(): void {
