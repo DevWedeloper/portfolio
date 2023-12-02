86649f22b4c420be841f056e0d3bfb7214d45c85
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-02 15:23:13 +0800

    Improved copy text mechanics by separating it to its own component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,21 +9,25 @@ import {
   inject,
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import {
-  FormBuilder,
-  ReactiveFormsModule
-} from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { SectionService } from '../shared/data-access/section.service';
 import { ThemeService } from '../shared/data-access/theme.service';
 import { ModalComponent } from '../shared/ui/components/modal/modal.component';
 import { ModalService } from '../shared/ui/components/modal/modal.service';
 import { ContactService } from './data-access/contact.service';
+import { CopyTextComponent } from './ui/copy-text/copy-text.component';
 import { FormComponent } from './ui/form/form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ModalComponent, FormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ModalComponent,
+    FormComponent,
+    CopyTextComponent,
+  ],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -37,6 +41,8 @@ export class ContactComponent implements OnInit {
   @ViewChild('section', { static: true }) section!: ElementRef<HTMLElement>;
   @ViewChild('thankYouTemplate') thankYouTemplate!: TemplateRef<HTMLElement>;
   @ViewChild('sorryTemplate') sorryTemplate!: TemplateRef<HTMLElement>;
+  email = 'vicnathangabrielle@gmail.com';
+  phoneNumber = '+63 965 558 5778';
 
   constructor() {
     this.cs.submitData$.pipe(takeUntilDestroyed()).subscribe({
@@ -51,34 +57,5 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
     this.ss.registerSection(this.section);
-  }
-
-  copyText(copyID: string, btnID: string): void {
-    const textElement = document.getElementById(copyID) as HTMLParagraphElement;
-    const textToCopy = textElement.textContent || '';
-    const copyBtnSpan = document.getElementById(btnID) as HTMLSpanElement;
-    const buttonImage = copyBtnSpan.querySelector('img') as HTMLImageElement;
-
-    const tempTextArea = document.createElement('textarea');
-    tempTextArea.value = textToCopy;
-    document.body.appendChild(tempTextArea);
-
-    tempTextArea.select();
-    document.execCommand('copy');
-
-    document.body.removeChild(tempTextArea);
-
-    buttonImage.src = 'assets/images/icons/check.svg';
-
-    copyBtnSpan.style.backgroundColor = 'var(--main-color)';
-
-    copyBtnSpan.removeAttribute('id');
-
-    setTimeout(() => {
-      buttonImage.src = 'assets/images/icons/copy.svg';
-      copyBtnSpan.style.backgroundColor = '';
-
-      copyBtnSpan.setAttribute('id', btnID);
-    }, 2000);
   }
 }
---
+++ src/app/contact/ui/copy-text/copy-text.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, HostListener, Input, ViewChild, inject } from '@angular/core';
+import { ThemeService } from 'src/app/shared/data-access/theme.service';
+import { Clipboard } from '@angular/cdk/clipboard';
+
+@Component({
+  selector: 'app-copy-text',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './copy-text.component.html',
+  styleUrls: ['./copy-text.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CopyTextComponent {
+  ts = inject(ThemeService);
+  elementRef = inject(ElementRef);
+  clipboard = inject(Clipboard);
+  @Input({ required: true }) text!: string;
+  @ViewChild('copyImage') copyImage!: ElementRef;
+  @HostBinding('tabindex') tabIndex = 0;
+  @HostListener('click')
+  @HostListener('keydown.Enter', ['$event'])
+  onEvent(): void {
+    this.clipboard.copy(this.text);
+    this.animateButton();
+  }
+
+  animateButton(): void {
+    this.copyImage.nativeElement.src = 'assets/images/icons/check.svg';
+    this.elementRef.nativeElement.style.backgroundColor = 'var(--main-color)';
+    setTimeout(() => {
+      this.copyImage.nativeElement.src = 'assets/images/icons/copy.svg';
+      this.elementRef.nativeElement.style.backgroundColor = '';
+    }, 2000);
+  }
+}
