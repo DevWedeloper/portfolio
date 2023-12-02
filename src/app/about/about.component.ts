41bb7229fa0da58f2a6167acd0529590cf69d4f8
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-03 03:02:24 +0800

    Separated tab content and experience content to another component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,43 +8,27 @@ import {
   inject
 } from '@angular/core';
 import { SectionService } from '../shared/data-access/section.service';
-import { ThemeService } from '../shared/data-access/theme.service';
-import { ModalComponent } from '../shared/ui/components/modal/modal.component';
-import { ModalService } from '../shared/ui/components/modal/modal.service';
-import { TabsComponent } from '../shared/ui/components/tabs/tabs.component';
-import { TooltipDirective } from '../shared/ui/components/tooltip/tooltip.directive';
-import { HighlightTextDirective } from '../shared/ui/directives/highlight-text.directive';
 import { AboutHeroImageComponent } from './ui/about-hero-image/about-hero-image.component';
+import { AboutTabComponent } from './ui/about-tab/about-tab.component';
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     CommonModule,
-    TabsComponent,
-    HighlightTextDirective,
-    TooltipDirective,
-    ModalComponent,
-    AboutHeroImageComponent
+    AboutHeroImageComponent,
+    AboutTabComponent
   ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AboutComponent implements OnInit {
-  ts = inject(ThemeService);
   sectionService = inject(SectionService);
-  ms = inject(ModalService);
   elementRef = inject(ElementRef);
   @HostBinding('attr.id') id = 'about';
   @HostBinding('class.section') wrapperClass = true;
-  tabs: string[] = ['Skills', 'Experience', 'Education'];
-  activatedTab = 'Skills';
 
   ngOnInit(): void {
     this.sectionService.registerSection(this.elementRef);
-  }
-  
-  tabChange(tabIndex: string) {
-    this.activatedTab = tabIndex;
   }
 }
---
+++ src/app/about/ui/cloudstaff-internship/cloudstaff-internship.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ThemeService } from 'src/app/shared/data-access/theme.service';
+import { ModalService } from 'src/app/shared/ui/components/modal/modal.service';
+import { TooltipDirective } from 'src/app/shared/ui/components/tooltip/tooltip.directive';
+
+@Component({
+  selector: 'app-cloudstaff-internship',
+  standalone: true,
+  imports: [CommonModule, TooltipDirective],
+  templateUrl: './cloudstaff-internship.component.html',
+  styleUrls: ['./cloudstaff-internship.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CloudstaffInternshipComponent {
+  ts = inject(ThemeService);
+  ms = inject(ModalService);
+}
