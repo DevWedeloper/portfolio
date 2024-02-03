92676e43ea9d02cd0026a4fc98fd9d7eed38666a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-02-03 13:55:41 +0800

    refactor: remove comments

--- src/app/shared/ui/components/modal/modal.service.ts
+++ src/app/shared/ui/components/modal/modal.service.ts
@@ -31,30 +31,23 @@ export class ModalService<T> {
   }
 
   open(contentTemplate: TemplateRef<HTMLElement>): void {
-    // Destroy previous modal if it exists
     if (this.modalComponentRef) {
       this.appRef.detachView(this.modalComponentRef.hostView);
       this.modalComponentRef?.destroy();
     }
 
-    // Create a component factory
     const factory =
       this.componentFactoryResolver.resolveComponentFactory(ModalComponent);
 
-    // Create a component reference
     this.modalComponentRef = factory.create(this.injector);
 
-    // Attach the component to the application ref so that it's part of the Angular application
     this.appRef.attachView(this.modalComponentRef.hostView);
 
-    // Get the DOM element from the component
     const domElem = (this.modalComponentRef.hostView as EmbeddedViewRef<T>)
       .rootNodes[0] as HTMLElement;
 
-    // Append the DOM element to the body
     document.body.appendChild(domElem);
 
-    // Pass the content template to the modal component
     this.modalComponentRef.instance.contentTemplate = contentTemplate;
 
     this.isOpen$.next(true);
@@ -64,7 +57,6 @@ export class ModalService<T> {
 
   close(): void {
     if (this.modalComponentRef) {
-      // Remove the modal component from the body
       this.appRef.detachView(this.modalComponentRef.hostView);
       this.modalComponentRef?.destroy();
       this.isBodyScrollDisabled = false;
