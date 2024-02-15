dd573cb6541b1c66e66e9f2652bec9225220963f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-02-15 14:42:23 +0800

    refactor: remove unused code

--- src/app/home/ui/home-hero-image/home-hero-image.component.ts
+++ src/app/home/ui/home-hero-image/home-hero-image.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-hero-image',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './home-hero-image.component.html',
   styleUrls: ['./home-hero-image.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
