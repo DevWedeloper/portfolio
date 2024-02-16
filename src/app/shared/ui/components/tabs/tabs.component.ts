d6ca2dfe927cd69c3d0922f7294f42880f690c1f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-02-16 13:43:51 +0800

    refactor: make activeTabElement a signal for better reactivity

--- src/app/shared/ui/components/tabs/tabs.component.ts
+++ src/app/shared/ui/components/tabs/tabs.component.ts
@@ -6,10 +6,9 @@ import {
   ElementRef,
   computed,
   contentChildren,
-  effect,
   signal,
   viewChild,
-  viewChildren
+  viewChildren,
 } from '@angular/core';
 import { TabDirective } from './tab.directive';
 
@@ -27,10 +26,10 @@ import { TabDirective } from './tab.directive';
 export class TabsComponent implements AfterViewInit {
   private line = viewChild.required<ElementRef<HTMLElement>>('line');
   private tabLinks = viewChildren<ElementRef<HTMLElement>>('tabLinks');
-  private activeTabElement: HTMLElement | undefined;
   protected tabs = contentChildren(TabDirective);
   protected activatedTab = computed(() => this.tabs()[0].title());
-  private activeTabId = signal<string | null>(null);
+  protected activeTabId = signal<string | null>(null);
+  private activeTabElement = signal<HTMLElement | null>(null);
 
   protected selectedTabTpl = computed(() => {
     const tabs = this.tabs();
@@ -43,19 +42,16 @@ export class TabsComponent implements AfterViewInit {
     return tabs.find((tab) => tab.title() === selected)!.template;
   });
 
-  constructor() {
-    effect(() => {
-      const initialActiveTab = this.tabLinks().find((el) =>
-        el.nativeElement.classList.contains('active'),
-      );
-      if (initialActiveTab) {
-        this.activeTabElement = initialActiveTab.nativeElement;
-        this.updateLinePosition();
-      }
-    });
-  }
+  protected linePosition = computed(() => {
+    const activeTab = this.activeTabElement();
+    const width = activeTab?.offsetWidth + 'px';
+    const left = activeTab?.offsetLeft + 'px';
+    const top = activeTab?.offsetHeight + 'px';
+    return { width, left, top };
+  });
 
   ngAfterViewInit(): void {
+    this.activeTabElement.set(this.tabLinks()[0].nativeElement);
     this.updateLinePosition();
   }
 
@@ -63,20 +59,19 @@ export class TabsComponent implements AfterViewInit {
     this.updateLinePosition();
   }
 
-  protected setTab(event: Event, tab: string): void {
+  protected setTab(event: Event, tab: string, el: HTMLElement): void {
     this.activeTabId.update(() => tab);
-    this.activeTabElement = event.currentTarget as HTMLElement;
-    this.updateLinePosition();
+    this.activeTabElement.set(el);
   }
 
   private updateLinePosition(): void {
-    if (this.activeTabElement) {
+    if (this.activeTabElement()) {
       this.line().nativeElement.style.width =
-        this.activeTabElement.offsetWidth + 'px';
+        this.activeTabElement()?.offsetWidth + 'px';
       this.line().nativeElement.style.left =
-        this.activeTabElement.offsetLeft + 'px';
+        this.activeTabElement()?.offsetLeft + 'px';
       this.line().nativeElement.style.top =
-        this.activeTabElement.offsetHeight + 'px';
+        this.activeTabElement()?.offsetHeight + 'px';
     }
   }
 }
