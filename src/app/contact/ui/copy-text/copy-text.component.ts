b64e2ee6134ddeb2b31394bea71a0ae73d66e99c
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-05 13:18:14 +0800

    refactor: use inline template/styles

--- src/app/contact/ui/copy-text/copy-text.component.ts
+++ src/app/contact/ui/copy-text/copy-text.component.ts
@@ -1,5 +1,5 @@
 import { Clipboard } from '@angular/cdk/clipboard';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, NgStyle } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -13,14 +13,41 @@ import { ThemeService } from '../../../shared/data-access/theme.service';
 @Component({
   selector: 'app-copy-text',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgStyle, AsyncPipe],
   host: {
     '[tabindex]': '0',
     '(click)': 'onEvent()',
     '(keydown.Enter)': 'onEvent()',
   },
-  templateUrl: './copy-text.component.html',
-  styleUrls: ['./copy-text.component.scss'],
+  template: `
+    <img
+      #copyImage
+      class="icon icon-size-adjust"
+      src="assets/images/icons/copy.svg"
+      alt="Copy Icon"
+      [ngStyle]="{
+        filter:
+          (ts.isDarkMode$ | async)
+            ? 'invert(100%) grayscale(100%)'
+            : 'grayscale(100%)',
+      }"
+    />
+  `,
+  styles: [
+    `
+      :host {
+        padding: 0.5rem;
+        margin: -0.5rem;
+        border-radius: 50%;
+        width: min-content;
+        height: min-content;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CopyTextComponent {
