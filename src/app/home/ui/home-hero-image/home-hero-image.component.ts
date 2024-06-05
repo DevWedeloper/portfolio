b80314709f823e4aa30fd88a5e43426809b53530
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-05 13:01:40 +0800

    refactor: use inline template/styles

--- src/app/home/ui/home-hero-image/home-hero-image.component.ts
+++ src/app/home/ui/home-hero-image/home-hero-image.component.ts
@@ -5,8 +5,49 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-home-hero-image',
   standalone: true,
   imports: [NgOptimizedImage],
-  templateUrl: './home-hero-image.component.html',
-  styleUrls: ['./home-hero-image.component.scss'],
+  template: `
+    <img
+      ngSrc="assets/images/backgrounds/me.webp"
+      fill
+      priority
+      alt="Hero Image"
+    />
+  `,
+  styles: [
+    `
+      :host {
+        position: relative;
+        width: var(--image-width-desktop);
+        height: var(--image-height-desktop);
+        border: 0.5rem solid var(--main-color);
+        animation: animate 5s ease-in-out infinite;
+        overflow: hidden;
+      }
+
+      img {
+        object-fit: cover;
+      }
+
+      @keyframes animate {
+        0% {
+          border-radius: 70% 30% 30% 70% / 60% 40% 60% 40%;
+        }
+        50% {
+          border-radius: 42% 61% 36% 66% / 65% 70% 28% 38%;
+        }
+        100% {
+          border-radius: 70% 30% 30% 70% / 60% 40% 60% 40%;
+        }
+      }
+
+      @media (max-width: 500px) {
+        :host {
+          width: var(--image-width-small-mobile);
+          height: var(--image-height-small-mobile);
+        }
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     '[class.home-img]': 'true',
