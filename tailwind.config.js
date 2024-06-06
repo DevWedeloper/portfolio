b80159bf402c4f1e417c7c33e60b6e64f37e4f0d
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-06 15:01:49 +0800

    feat: create new tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'max-lg': {'max': '991px'},
+        'max-md': {'max': '768px'},
+        'max-sm': {'max': '500px'},
+      },
+      colors: {
+        'main-color': 'var(--main-color)',
+        'primary-color': 'var(--primary-color)',
+        'secondary-color': 'var(--secondary-color)',
+        'text-color': 'var(--text-color)',
+        'text-color-reverse': 'var(--text-color-reverse)',
+      },
+      fontSize: {
+        'big': 'var(--font-size-big)',
+        'regular': 'var(--font-size-regular)',
+        'tooltip': 'var(--font-size-tooltip)',
+      },
+      spacing: {
+        'icon-adjust': 'var(--icon-size-adjust)',
+        'image-width': 'var(--image-width)',
+        'image-height': 'var(--image-height)',
+      },
+    },
   },
   plugins: [],
 }
