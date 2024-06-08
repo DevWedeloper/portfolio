b79316e14bc8dc5f3ed0f8ae4288002e9c77f2bd
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-08 21:46:51 +0800

    refactor: use tailwind

--- src/app/contact/ui/feedback-success/feedback-success.component.ts
+++ src/app/contact/ui/feedback-success/feedback-success.component.ts
@@ -1,20 +1,32 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CustomButtonComponent } from '../../../shared/ui/components/custom-button/button';
 import { ModalService } from '../../../shared/ui/components/modal/modal.service';
+import {
+  feedback,
+  feedbackButton,
+  feedbackHeading,
+  feedbackIcon,
+  feedbackText,
+} from '../feedback';
 
 @Component({
   selector: 'app-feedback-success',
   standalone: true,
   imports: [CustomButtonComponent],
   template: `
-    <div class="submit-modal-content">
-      <img src="assets/images/icons/heart-eyes.svg" alt="Heart Eyes Emoji" />
-      <h5>Thank You!</h5>
-      <p>
+    <div class="${feedback}">
+      <img
+        class="${feedbackIcon}"
+        src="assets/images/icons/heart-eyes.svg"
+        alt="Heart Eyes Emoji"
+      />
+      <h5 class="${feedbackHeading}">Thank You!</h5>
+      <p class="${feedbackText}">
         The message was sent. Your feedback will help shape me to become a
         better developer. Thanks!
       </p>
       <button
+        class="${feedbackButton}"
         custom-button
         disableEffect
         (click)="ms.close()"
@@ -24,7 +36,6 @@ import { ModalService } from '../../../shared/ui/components/modal/modal.service'
       </button>
     </div>
   `,
-  styleUrls: ['./../feedback.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeedbackSuccessComponent {
---
+++ src/app/contact/ui/feedback.ts
@@ -0,0 +1,10 @@
+export const feedback =
+  'relative w-[95vw] max-w-[400px] p-8 text-center text-regular';
+
+export const feedbackIcon = '-mt-[50%] w-24';
+
+export const feedbackHeading = 'p-4';
+
+export const feedbackText = 'mb-1';
+
+export const feedbackButton = 'w-full p-4';
