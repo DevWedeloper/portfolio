1ffb0ccb11800c582eca6c1139a95b2d4fd4e19b
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-09 16:01:36 +0800

    refactor: improve theme architecture

--- src/app/shared/data-access/theme.service.ts
+++ src/app/shared/data-access/theme.service.ts
@@ -1,7 +1,9 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { isPlatformBrowser } from '@angular/common';
 import {
   Injectable,
+  PLATFORM_ID,
   RendererFactory2,
-  afterNextRender,
   inject,
 } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -10,16 +12,18 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ThemeService {
+  private _platformId = inject(PLATFORM_ID);
   private renderer = inject(RendererFactory2).createRenderer(null, null);
-  private darkThemeMediaQuery!: MediaQueryList;
+  private darkThemeMediaQuery = inject(MediaMatcher).matchMedia(
+    '(prefers-color-scheme: dark)',
+  );
   darkMode$ = new BehaviorSubject<boolean>(true);
   isDarkMode$: Observable<boolean> = this.darkMode$.asObservable();
 
   constructor() {
-    afterNextRender(() => {
-      this.darkThemeMediaQuery = matchMedia('(prefers-color-scheme: dark)');
-      this.checkPreferredTheme();
-    });
+    this.darkThemeMediaQuery.onchange = (event: MediaQueryListEvent) =>
+      this.darkMode$.next(event.matches ? true : false);
+    this.syncInitialStateFromLocalStorage();
   }
 
   themeOnClick(): void {
@@ -33,11 +37,13 @@ export class ThemeService {
     localStorage.setItem('preferredTheme', preferredTheme);
   }
 
-  checkPreferredTheme(): void {
-    const preferredTheme = localStorage.getItem('preferredTheme');
-    const isDarkTheme =
-      preferredTheme === 'dark' ||
-      (preferredTheme === null && this.darkThemeMediaQuery.matches);
-    this.darkMode$.next(isDarkTheme);
+  private syncInitialStateFromLocalStorage(): void {
+    if (isPlatformBrowser(this._platformId)) {
+      const preferredTheme = localStorage.getItem('preferredTheme');
+      const isDarkTheme =
+        preferredTheme === 'dark' ||
+        (preferredTheme === null && this.darkThemeMediaQuery.matches);
+      this.darkMode$.next(isDarkTheme);
+    }
   }
 }
