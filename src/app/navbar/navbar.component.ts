8638127a5f238c71444cd87a7a511c8945b3c407
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-18 00:35:03 +0800

    feat: improve html semantics

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -17,33 +17,35 @@ import { NavbarThemeComponent } from './navbar-theme/navbar-theme.component';
     NavbarMobileComponent,
   ],
   template: `
-    <nav
+    <div
       class="fixed z-10 flex h-12 w-full items-center justify-between px-[9%] py-8 text-text-color before:absolute before:inset-0 before:-z-10 before:backdrop-blur-sm max-lg:px-[3%] max-md:bg-primary-color"
     >
       <h1 class="text-3xl">Nathan.</h1>
 
-      <app-navbar-mobile class="navbar-mobile hidden max-md:flex" />
+      <nav>
+        <app-navbar-mobile class="navbar-mobile hidden max-md:flex" />
 
-      <ul class="flex max-md:hidden">
-        @for (section of sections(); track $index) {
-          <li
-            class="flex px-6 [&:nth-last-child(2)]:border-r-4 [&:nth-last-child(2)]:[border-right:solid]"
-          >
-            <a
-              [routerLink]="[]"
-              [fragment]="section.id"
-              [ngClass]="{ 'text-main-color': section.isActive }"
-              class="-m-2 rounded-lg p-2 text-regular hover:text-main-color hover:backdrop-blur-sm"
+        <ul class="flex max-md:hidden">
+          @for (section of sections(); track $index) {
+            <li
+              class="flex px-6 [&:nth-last-child(2)]:border-r-4 [&:nth-last-child(2)]:[border-right:solid]"
             >
-              {{ section.id | upperFirst }}
-            </a>
+              <a
+                [routerLink]="[]"
+                [fragment]="section.id"
+                [ngClass]="{ 'text-main-color': section.isActive }"
+                class="-m-2 rounded-lg p-2 text-regular hover:text-main-color hover:backdrop-blur-sm"
+              >
+                {{ section.id | upperFirst }}
+              </a>
+            </li>
+          }
+          <li class="flex px-6 pr-0">
+            <app-navbar-theme />
           </li>
-        }
-        <li class="flex px-6 pr-0">
-          <app-navbar-theme />
-        </li>
-      </ul>
-    </nav>
+        </ul>
+      </nav>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
