af0bc9b0aa287e2b5c22520d748ebcdc0cb2c847
DevWedeloper <vicnathangabrielle@gmail.com>
2025-07-13 08:42:08 +0800

    fix: canonical issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://devwedeloper.vercel.app',
   vite: {
     plugins: [tailwindcss()],
   },
