d0b5f96c927e4650c2e49495de6edad82f9f32ed
DevWedeloper <vicnathangabrielle@gmail.com>
2025-07-14 05:52:32 +0800

    feat: install rollup plugin visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://devwedeloper.vercel.app',
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      visualizer({
+        emitFile: true,
+        filename: 'stats.html',
+      }),
+    ],
   },
   integrations: [mdx(), svelte()],
 });
