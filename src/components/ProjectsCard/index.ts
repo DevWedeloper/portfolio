a3a65e4231b73755f0aaf8fde41ce00f9bec467f
DevWedeloper <vicnathangabrielle@gmail.com>
2026-03-13 23:45:00 +0800

    feat: create automation projects card

--- src/components/ProjectsCard/index.ts
+++ src/components/ProjectsCard/index.ts
@@ -1,1 +1,2 @@
-export { default } from './ProjectsCard.astro'
+export { default as AutomationProjectsCard } from './AutomationProjectsCard.astro'
+export { default as WebsiteProjectsCard } from './WebsiteProjectsCard.astro'
